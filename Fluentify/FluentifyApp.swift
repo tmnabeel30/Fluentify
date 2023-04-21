5eeacf1aaa897816baa2a8f44b9a6672ced8774a
tmnabeel30 <tmnabeel30@gmail.com>
2023-04-22 03:47:26 +0530

    Initial Commit

---
+++ Fluentify/FluentifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluentifyApp.swift
+//  Fluentify
+//
+//  Created by Nabeel on 22/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FluentifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
