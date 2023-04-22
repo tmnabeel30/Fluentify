03ba5e26dec3f5e13b836a2c3241b2e0dae6d925
tmnabeel30 <tmnabeel30@gmail.com>
2023-04-22 16:16:22 +0530

    UI majorly done

--- Fluentify/ContentView.swift
+++ Fluentify/ContentView.swift
@@ -5,99 +5,11 @@
 //  Created by Nabeel on 22/04/23.
 //
 
+
 import SwiftUI
-
-struct ContentView: View {
-    
-    
-    @State private var message = ""
-      @State private var messages = ["Hello! How can I assist you today?"]
-      @State private var keyboardHeight: CGFloat = 0
-      
-      var body: some View {
-          VStack {
-              ScrollView {
-                  ForEach(messages, id: \.self) { message in
-                      if message.contains("user") {
-                          HStack {
-                              Spacer()
-                              Text(message)
-                                  .padding()
-                                  .background(Color.blue)
-                                  .foregroundColor(.white)
-                                  .clipShape(ChatBubble(isFromCurrentUser: true))
-                          }
-                      } else {
-                          HStack {
-                              Text(message)
-                                  .padding()
-                                  .background(Color.gray)
-                                  .foregroundColor(.white)
-                                  .clipShape(ChatBubble(isFromCurrentUser: false))
-                              Spacer()
-                          }
-                      }
-                  }
-              }
-              HStack {
-                  TextField("Type a message...", text: $message)
-                      .textFieldStyle(RoundedBorderTextFieldStyle())
-                      .padding()
-                      .background(Color.white)
-                      .clipShape(Capsule())
-                      .onTapGesture {
-                          addKeyboardObserver()
-                      }
-                  Button("Send") {
-                      messages.append("user: " + message)
-                      message = ""
-                      removeKeyboardObserver()
-                  }
-                  .padding()
-                  .foregroundColor(.white)
-                  .background(Color.blue)
-                  .clipShape(Circle())
-              }
-              .padding()
-              .background(Color.gray.opacity(0.1))
-              .offset(y: -keyboardHeight)
-              .animation(.easeInOut(duration: 0.3))
-          }
-          .edgesIgnoringSafeArea(.bottom)
-          .onTapGesture {
-              hideKeyboard()
-          }
-      }
-      
-      func addKeyboardObserver() {
-          NotificationCenter.default.addObserver(forName: UIResponder.keyboardWillShowNotification, object: nil, queue: .main) { (notification) in
-              guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
-              keyboardHeight = keyboardFrame.height
-          }
-      }
-      
-      func removeKeyboardObserver() {
-          NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
-      }
-      
-      func hideKeyboard() {
-          UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-      }
-  }
-
-
-struct ChatBubble: Shape {
-    var isFromCurrentUser: Bool
-    
-    func path(in rect: CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [.topLeft, .topRight, isFromCurrentUser ? .bottomLeft : .bottomRight], cornerRadii: CGSize(width: 16, height: 16))
-        return Path(path.cgPath)
-    }
-}
-
 struct ChatView: View {
     @State private var message = ""
-    @State private var messages = ["Hello! How can I assist you today?"]
+    @State private var messages = ["AI: Hello! How can I assist you today?"]
     @State private var keyboardHeight: CGFloat = 0
     
     var body: some View {
@@ -113,7 +25,7 @@ struct ChatView: View {
                                 .foregroundColor(.white)
                                 .clipShape(ChatBubble(isFromCurrentUser: true))
                         }
-                    } else {
+                    } else if message.contains("AI"){
                         HStack {
                             Text(message)
                                 .padding()
@@ -170,9 +82,17 @@ struct ChatView: View {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ChatBubble: Shape {
+    var isFromCurrentUser: Bool
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [.topLeft, .topRight, isFromCurrentUser ? .bottomLeft : .bottomRight], cornerRadii: CGSize(width: 16, height: 16))
+        return Path(path.cgPath)
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ChatView()
+        }
     }
 }
