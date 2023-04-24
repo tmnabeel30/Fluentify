c2986e22274fd1e6be703cbfcbab405fbdf0e701
tmnabeel30 <tmnabeel30@gmail.com>
2023-04-24 20:47:16 +0530

    openai implemented

--- Fluentify/ContentView.swift
+++ Fluentify/ContentView.swift
@@ -7,10 +7,45 @@
 
 
 import SwiftUI
+import OpenAI
+
+struct CompletionsQuery: Codable {
+    /// ID of the model to use.
+    let model: Model
+    /// The prompt(s) to generate completions for, encoded as a string, array of strings, array of tokens, or array of token arrays.
+    let prompt: String
+    /// What sampling temperature to use. Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer.
+    let temperature: Int
+    /// The maximum number of tokens to generate in the completion.
+    let max_tokens: Int
+    /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+    let top_p: Int
+    /// Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+    let frequency_penalty: Int
+    /// Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
+    let presence_penalty: Int
+    /// Up to 4 sequences where the API will stop generating further tokens. The returned text will not contain the stop sequence.
+    let stop: [String]
+}
+struct CompletionsResult: Codable {
+    struct Choice: Codable {
+        let text: String
+        let index: Int
+    }
+
+    let id: String
+    let object: String
+    let created: TimeInterval
+    let model: Model
+    let choices: [Choice]
+}
+
 struct ChatView: View {
     @State private var message = ""
     @State private var messages = ["AI: Hello! How can I assist you today?"]
     @State private var keyboardHeight: CGFloat = 0
+    let openai = OpenAI(apiToken: "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")
+    
     
     var body: some View {
         VStack {
@@ -48,8 +83,14 @@ struct ChatView: View {
                     }
                 Button("Send") {
                     messages.append("user: " + message)
-                    message = ""
+                    
                     removeKeyboardObserver()
+                    openai.completions(query: .init(model: .textDavinci_003, prompt: message, temperature: 0, maxTokens: 100, topP: 1, frequencyPenalty: 0, presencePenalty: 0, stop: ["\\n"])) { result in
+                      //Handle response here
+                        print(CompletionsResult.Choice(text: <#T##String#>, index: 0)
+                        
+                        message = ""
+                    }
                 }
                 .padding()
                 .foregroundColor(.white)
